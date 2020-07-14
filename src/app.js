5bf3bea359db623c28fe6923496f669b02ab151b
rohitray05 <rohit.kmr5313@gmail.com>
2020-07-14 22:25:07 +0530

    Change message added

--- src/app.js
+++ src/app.js
@@ -64,13 +64,13 @@ app.get('/weather',(req,res)=>{
     geocode(req.query.address,(error,{latitude,longitude,location}={})=>{
         if(error){
           return res.send({
-            error:'Error Fetching Location'
+            error
         })
         }
         forecast(latitude,longitude,(error,forecastData)=>{
           if(error){
             return res.send({
-                error:'Error Fetching ForeCast'
+                error
             })
           }
           
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,15 +1,16 @@
 const request = require('request');
 
 const forecast = (latitude,longitude,callback)=>{
-    const url = `https://api.darksky.net/forecast/e01ce6a8c6e6e4cc3c0ec1a68c9a948f/${latitude},${longitude}?units=si`;
+    const url = `http://api.weatherstack.com/current?access_key=96719aa75662d91f2346ea448b535a39&query=${latitude},${longitude}`;
     request({url,json:true},(error,{body})=>{
         if(error){
           callback('Unable to connect to location services',undefined)
         }else if(body.error){
-         console.log('Unable to find location',undefined);
+          callback('Unable to find location',undefined);
         }else{
          //const data  = JSON.parse(response.body)
-         callback(undefined,`It is Currently ${body.currently.temperature} degree out. There is ${body.currently.precipProbability} % chance of rain `);
+         callback(undefined,`It is Currently ${body.current.temperature} degree out there,but it feels like ${body.current.feelslike}. There is ${body.current.precip} % chance of rain 
+         and ${body.current.weather_descriptions}!`);
         }
          
      })
